96c037ab11180bb21b10e994f83f17c490f0920a
Macbook <macbook@Macbooks-MacBook-Pro.local>
2025-01-26 02:31:35 +0100

    Initial commit: Spring Initializr project setup

---
+++ src/main/kotlin/com/slaxation/kotlinMoro/KotlinMoroApplication.kt
@@ -0,0 +1,11 @@
+package com.slaxation.kotlinMoro
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinMoroApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinMoroApplication>(*args)
+}
---
+++ src/test/kotlin/com/slaxation/kotlinMoro/KotlinMoroApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.slaxation.kotlinMoro
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinMoroApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
